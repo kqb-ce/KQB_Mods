bb5b60544bf9a37261b4be64d5409b5bb90da7d1
treebones <billy@LAPTOP-E6Q1J4LO.>
2024-08-25 17:31:37 -0700

    add kqb-o-ween

---
+++ Source/KQBMods/KQB-o-ween/Halloween.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+namespace KQB_o_ween
+{
+	[HarmonyPatch(typeof(AssetSystem), MethodType.Constructor)]
+	internal class AssetSystemMod
+	{
+		private static void Postfix(AssetSystem __instance)
+		{
+			AssetSystem.bundlePlayerSpritesName = "../../BepInEx/plugins/halloween/assets_playersprites-halloween";
+			AssetSystem.bundleSharedUIName = "../../BepInEx/plugins/halloween/assets_sharedui-halloween";
+		}
+	}
+}
---
+++ Source/KQBMods/KQB-o-ween/HalloweenPlugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace KQB_o_ween
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class HalloweenPlugin : BaseUnityPlugin
+    {
+        private const string myGUID = "com.treebones.halloween";
+        private const string pluginName = "Halloween";
+        private const string versionString = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        public static ManualLogSource logger;
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            logger = Logger;
+        }
+    }
+}
