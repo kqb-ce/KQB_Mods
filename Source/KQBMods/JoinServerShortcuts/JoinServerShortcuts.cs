b2d9b275604e8e8ee2c503f3f9ba7eff81d2ba6c
its-treebones2 <bdwelch@protonmail.com>
2024-08-30 13:11:10 -0700

    add JoinServerShortcuts

---
+++ Source/KQBMods/JoinServerShortcuts/JoinServerShortcuts.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using LiquidBit.KillerQueenX;
+using System.Reflection;
+using UnityEngine;
+
+namespace SteamlessClientMod
+{
+    [HarmonyPatch(typeof(DevConCommands))]
+    [HarmonyPatch("JoinServer")]
+    public static class JoinServer_Patch
+    {
+        public static bool Prefix(DevConCommands __instance, string[] p)
+        {
+          if (p.Length >= 2)
+		  {   
+            return true;
+          }
+          string jsonFilePath = @"servers.json";
+        
+          string json = File.ReadAllText(jsonFilePath);
+          Dictionary<string, CommunityServerConnection> json_Dictionary = (new JavaScriptSerializer()).Deserialize<Dictionary<string, CommunityServerConnection>>(json);
+
+          CommunityServerConnection conn = json_Dictionary.getField(p[0])
+          UIManager.Instance.DirectConnectToServer(conn.ip, conn.port, loopback: false);
+          return false;
+        }
+    }
+
+    public class CommunityServerConnection
+    {
+        public ushort port;
+        public string ip;
+    }
+
+}
---
+++ Source/KQBMods/JoinServerShortcuts/SteamlessClientModPlugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace LosersCanDanceMod
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class JoinServerShortcutsPlugin : BaseUnityPlugin
+    {
+        private const string myGUID = "com.treebones.JoinServerShortcuts";
+        private const string pluginName = "JoinServer Shortcuts";
+        private const string versionString = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        public static ManualLogSource logger;
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            logger = Logger;
+        }
+    }
+}
