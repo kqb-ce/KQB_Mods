295b5c90bd6da31ddfd8b2519cf65af2b05f2ee1
its-treebones2 <bdwelch@protonmail.com>
2024-08-30 13:51:00 -0700

    add JoinServerShortcuts

--- Source/KQBMods/JoinServerShortcuts/JoinServerShortcuts.cs
+++ Source/KQBMods/JoinServerShortcuts/JoinServerShortcuts.cs
@@ -7,22 +7,27 @@ namespace SteamlessClientMod
 {
     [HarmonyPatch(typeof(DevConCommands))]
     [HarmonyPatch("JoinServer")]
+    public static Dictionary<string, CommunityServerConnection> m_CommunityServerDict;
     public static class JoinServer_Patch
     {
         public static bool Prefix(DevConCommands __instance, string[] p)
         {
-          if (p.Length >= 2)
-		  {   
+            if (p.Length >= 2)
+            {   
             return true;
-          }
-          using (WebClient wc = new WebClient())
-          {
-            string json = wc.DownloadString("https://kqbfileserver.fly.dev/servers.json");
-            Dictionary<string, CommunityServerConnection> json_Dictionary = (new JavaScriptSerializer()).Deserialize<Dictionary<string, CommunityServerConnection>>(json);
+            }
 
-            CommunityServerConnection conn = json_Dictionary.getField(p[0])
+            if( m_CommunityServerDict == null)
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    string json = wc.DownloadString("https://kqbfileserver.fly.dev/servers.json");
+                    m_CommunityServerDict = (new JavaScriptSerializer()).Deserialize<Dictionary<string, CommunityServerConnection>>(json);
+                }
+            }
+            CommunityServerConnection conn = m_CommunityServerDict.getField(p[0]);
             UIManager.Instance.DirectConnectToServer(conn.ip, conn.port, loopback: false);
-          }        
+                  
           return false;
         }
     }
